d4ebb4d3ea2ddf93c8637a1011f54218ba8c258d
Joana <account@joana.cc>
2024-03-11 18:20:48 +0000

    Progress

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
-const buttons = document.querySelectorAll('.board-dots button');
+const buttons = document.querySelectorAll(".board-dots button");
+let userTurn = 0;
+let inactiveButtonsList = [];
+let availableInactiveButtons = [];
+//const adjacencyList = [  ["R1C2", "R1C3"],  ["R1C2", "R2C2"],  ["R1C3", "R2C3"],  ["R2C1", "R2C2"],  ["R2C1", "R3C1"],  ["R2C2", "R2C3"],  ["R2C2", "R3C2"],  ["R2C3", "R2C4"],  ["R2C3", "R3C3"],  ["R2C4", "R3C4"],  ["R3C1", "R3C2"],  ["R3C2", "R3C3"],  ["R3C2", "R4C2"],  ["R3C3", "R3C4"],  ["R3C3", "R4C3"],];
 
+const adjacencies = [
+  ["R1C2", "R1C3"],
+  ["R1C2", "R2C2"],
+  ["R1C3", "R2C3"],
+  ["R2C1", "R2C2"],
+  ["R2C1", "R3C1"],
+  ["R2C2", "R2C3"],
+  ["R2C2", "R3C2"],
+  ["R2C3", "R2C4"],
+  ["R2C3", "R3C3"],
+  ["R2C4", "R3C4"],
+  ["R3C1", "R3C2"],
+  ["R3C2", "R3C3"],
+  ["R3C2", "R4C2"],
+  ["R3C3", "R3C4"],
+  ["R3C3", "R4C3"],
+];
+
+//const adjacencies = ["R1C2 R1C3","R1C2 R2C2", "R1C3 R2C3", "R2C1 R2C2", "R2C1 R3C1", "R2C2 R2C3", "R2C2 R3C2", "R2C3 R2C4", "R2C3 R3C3", "R2C4 R3C4", "R3C1 R3C2", "R3C2 R3C3", "R3C2 R4C2", "R3C3 R3C4", "R3C3 R4C3"];
+
+computerTurn = () => {
+  let activeButtons = document.querySelectorAll("button.active");
+  let inactiveButtons = document.querySelectorAll("button:not(.active)");
+  let possibleMatches = [];
+  console.log(inactiveButtons.length);
+
+  inactiveButtons.forEach((inactiveButton) => {
+    let inactiveButtonID = inactiveButton.getAttribute("id");
+    for (i = 0; i < adjacencies.length; i++) {
+      if (adjacencies[i].includes(inactiveButtonID)) {
+        activeButtons.forEach((activeButton) => {
+          let activeButtonID = activeButton.getAttribute('id');
+          if (adjacencies[i].includes(activeButtonID)) {
+            console.log(adjacencies[i] + " includes " + inactiveButtonID + ' and ' + activeButtonID);
+            possibleMatches.push(inactiveButtonID);
+          }
+        });
+      }
+    }
+  });
+  console.log(possibleMatches);
+  let randomMatch = possibleMatches[Math.floor(Math.random()*possibleMatches.length)];
+  inactiveButtons.forEach((inactiveButton) => {
+    let inactiveButtonID = inactiveButton.getAttribute("id");
+  if(inactiveButtonID === randomMatch){
+    inactiveButton.classList.add('active');
+  }
+
+  })
+};
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        alert('clicked!');
-    })
-})
+  button.addEventListener("click", () => {
+    //if (userTurn === 0) {
+    userTurn++;
+    button.classList.add("active");
+    computerTurn();
+    //}
+  });
+});
