a47d29f73af6bd8e62da7063c083fc40aabc8ba0
Joana <account@joana.cc>
2024-03-15 16:33:43 +0000

    WIP

--- script.js
+++ script.js
@@ -32,12 +32,18 @@ computerTurn = () => {
 
   inactiveButtons.forEach((inactiveButton) => {
     let inactiveButtonID = inactiveButton.getAttribute("id");
-    for (i = 0; i < adjacencies.length; i++) {
+    for (let i = 0; i < adjacencies.length; i++) {
       if (adjacencies[i].includes(inactiveButtonID)) {
         activeButtons.forEach((activeButton) => {
-          let activeButtonID = activeButton.getAttribute('id');
+          let activeButtonID = activeButton.getAttribute("id");
           if (adjacencies[i].includes(activeButtonID)) {
-            console.log(adjacencies[i] + " includes " + inactiveButtonID + ' and ' + activeButtonID);
+            console.log(
+              adjacencies[i] +
+                " includes " +
+                inactiveButtonID +
+                " and " +
+                activeButtonID
+            );
             possibleMatches.push(inactiveButtonID);
           }
         });
@@ -45,21 +51,59 @@ computerTurn = () => {
     }
   });
   console.log(possibleMatches);
-  let randomMatch = possibleMatches[Math.floor(Math.random()*possibleMatches.length)];
+  let randomMatch =
+    possibleMatches[Math.floor(Math.random() * possibleMatches.length)];
   inactiveButtons.forEach((inactiveButton) => {
     let inactiveButtonID = inactiveButton.getAttribute("id");
-  if(inactiveButtonID === randomMatch){
-    inactiveButton.classList.add('active');
-  }
-
-  })
+    if (inactiveButtonID === randomMatch) {
+      inactiveButton.classList.add("active");
+      inactiveButton.classList.add("pc");
+    }
+  });
 };
+
+checkAdjacencies = (el, ef) => {
+  let matching = 0;
+  for (let i = 0; i < adjacencies.length; i++) {
+    //if adjacency entry includes this button
+    if (adjacencies[i].includes(el)) {
+      //get all active buttons
+      let activeButtons = document.querySelectorAll("button.active");
+      activeButtons.forEach((activeButton) => {
+        let activeButtonID = activeButton.getAttribute("id");
+        //if adjacency entry includes any active button
+
+        if (adjacencies[i].includes(activeButtonID)) {
+          console.log("there is a match in " + adjacencies[i]);
+          matching = 1;
+        }
+      });
+    }
+  }
+  if (matching === 1) {
+    ef.classList.add("active");
+  } else {
+    alert("Please choose a point connected to an already active one.");
+  }
+};
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    //if (userTurn === 0) {
-    userTurn++;
-    button.classList.add("active");
-    computerTurn();
-    //}
+    let currentButtonID = button.getAttribute("id");
+    if (userTurn === 0) {
+      userTurn++;
+      button.classList.add("active");
+      computerTurn();
+    } else {
+      if (button.classList.contains("active")) {
+        console.log("This is already active");
+      } else {
+        console.log("this button's ID: " + currentButtonID);
+        checkAdjacencies(currentButtonID, button);
+        if (button.classList.contains("active")) {
+          computerTurn();
+        }
+      }
+    }
   });
 });
