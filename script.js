d53188ec47de17b03a7f17d9144161c3e6b5ea97
Joana <account@joana.cc>
2024-03-18 00:39:52 +0000

    finish!

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const buttons = document.querySelectorAll(".board-dots button");
+const lines = document.querySelectorAll(".line");
 let firstTurn = true;
 let userWin = false;
 let lastPickedID = "";
+let messageBoard = document.getElementById("message-board");
+let resetButton = document.getElementById("reset-btn");
 
 const adjacencies = [
   ["R1C2", "R1C3"],
@@ -22,8 +25,25 @@ const adjacencies = [
   ["R4C2", "R4C3"],
 ];
 
+// reset / restart the game
+resetButton.addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("user");
+    button.classList.remove("pc");
+    button.dataset.status = "";
+    lastPickedID = "";
+    messageBoard.innerHTML = "Your turn!";
+    userWin = false;
+    firstTurn = true;
+  });
+  lines.forEach((line) => {
+    line.classList.remove("active");
+  });
+});
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    messageBoard.innerHTML = "Your turn!";
     let currentButtonID = button.getAttribute("id");
 
     // if it's the first turn, any vertex can be picked;
@@ -40,12 +60,11 @@ buttons.forEach((button) => {
         button.classList.contains("user") ||
         button.dataset.status === "last-picked"
       ) {
-        alert(
-          "This vertex was already picked. Choose an adjacent to the last one picked."
-        );
+        messageBoard.innerHTML =
+          "This vertex was already picked. <br/> Choose an adjacent to the last one picked.";
       } else {
         let match = false;
-        // check adjacencies list for one that matches the button and the last picked one;
+        // check adjacencies list for one that matches the current button and the last picked one;
         let lastPicked = document.querySelector('[data-status="last-picked"]');
         for (let i = 0; i < adjacencies.length; i++) {
           if (
@@ -57,25 +76,33 @@ buttons.forEach((button) => {
           }
         }
         if (match === true) {
-          button.dataset.status = "last-picked";
-          button.classList.add("user");
-          computerTurn(currentButtonID, button);
-          if ((userWin === false)) {
+          button.classList.add("user"); // add user colour;
+          button.dataset.status = "last-picked"; // mark it as the last picked vertex;
+          connectVertexes(currentButtonID); // activate connecting line;
+
+          computerTurn(currentButtonID); //let computer play;
+
+          // Check who wins;
+          if (userWin === false) {
             button.dataset.status = "";
             checkUserPlays();
           }
         } else {
-          alert("Choose a vertex adjacent to the last one picked.");
+          messageBoard.innerHTML =
+            "Choose a vertex adjacent to the last one picked.";
         }
       }
     }
   });
 });
 
+//let computer play;
 computerTurn = (e) => {
   let availableVertexes = document.querySelectorAll("[data-status]");
   let possibleMatches = [];
+  lastPickedID = e;
   availableVertexes.forEach((availableVertex) => {
+    // filter available vertexes;
     if (
       availableVertex.dataset.status === "" &&
       !(
@@ -84,6 +111,7 @@ computerTurn = (e) => {
       )
     ) {
       let availableVertexID = availableVertex.getAttribute("id");
+      // verify adjacencies with available the vertexes for possible matches;
       for (let i = 0; i < adjacencies.length; i++) {
         if (
           adjacencies[i].includes(availableVertexID) &&
@@ -94,7 +122,10 @@ computerTurn = (e) => {
       }
     }
   });
+
+  //check for available matches
   if (possibleMatches.length >= 1) {
+    // pick a random match for the last picked vertex;
     let randomMatch =
       possibleMatches[Math.floor(Math.random() * possibleMatches.length)];
     availableVertexes.forEach((availableVertex) => {
@@ -102,17 +133,24 @@ computerTurn = (e) => {
       if (availableVertexID === randomMatch) {
         availableVertex.dataset.status = "last-picked";
         availableVertex.classList.add("pc");
+        console.log("available random vertex: " + availableVertexID);
+        console.log("last picked vertex: " + lastPickedID);
+
+        // activate connecting line;
+        connectVertexes(availableVertexID);
+
+        // update the last picked vertex;
         lastPickedID = availableVertexID;
       }
     });
   } else {
     userWin = true;
-    alert("User wins!");
+    messageBoard.innerHTML = "User wins!";
   }
 };
 
+// Check if there are available vertexes for the user to play;
 checkUserPlays = () => {
-  // Check if there are available vertexes for the user to play
   let remainingUserPlays = 0;
   let availableVertexes = document.querySelectorAll("[data-status]");
   availableVertexes.forEach((availableVertex) => {
@@ -133,7 +171,18 @@ checkUserPlays = () => {
       }
     }
   });
+
+  // if not, then computer wins;
   if (remainingUserPlays === 0) {
-    alert("Computer wins!");
+    messageBoard.innerHTML = "Computer wins!";
   }
+};
+
+connectVertexes = (cid) => {
+  lines.forEach((line) => {
+    if (line.classList.contains(cid) && line.classList.contains(lastPickedID)) {
+      console.log("it connects!");
+      line.classList.add("active");
+    }
+  });
 };
